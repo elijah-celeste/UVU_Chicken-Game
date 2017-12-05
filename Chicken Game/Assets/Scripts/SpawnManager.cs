18484617f94cefdbf9132dc864978c9b0d15f808
Unknown <elijah.celeste@gmail.com>
2017-12-04 17:46:38 -0700

    Organized Hierarchy

--- Chicken Game/Assets/Scripts/SpawnManager.cs
+++ Chicken Game/Assets/Scripts/SpawnManager.cs
@@ -2,13 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyManager : MonoBehaviour {
+public class SpawnManager : MonoBehaviour {
 
 	public GameObject enemy;
 	public float spawnTime = 3f;
 	public Transform[] spawnPoints;
 
-	void Spawn () {
+	public void Spawn () {
 		int spawnPointIndex = Random.Range (0,spawnPoints.Length);
 
 		Instantiate (enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
