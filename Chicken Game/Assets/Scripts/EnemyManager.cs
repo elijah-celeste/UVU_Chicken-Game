f6ad85b264c06e08ee8fdd6c08d06945120e4c7f
Unknown <elijah.celeste@gmail.com>
2017-12-04 17:36:51 -0700

    Added spawners

---
+++ Chicken Game/Assets/Scripts/EnemyManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour {
+
+	public GameObject enemy;
+	public float spawnTime = 3f;
+	public Transform[] spawnPoints;
+
+	void Spawn () {
+		int spawnPointIndex = Random.Range (0,spawnPoints.Length);
+
+		Instantiate (enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+	}
+}
